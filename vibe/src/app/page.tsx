294539bd12ef398ad70e87ed8d75400282775b4d
Armin Banfalvi <abanfalvi@yahoo.com>
2025-11-07 23:11:17 +0100

    04: bacground-jobs

--- vibe/src/app/page.tsx
+++ vibe/src/app/page.tsx
@@ -1,23 +1,26 @@
-import { caller, getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
+"use client";
 
+import {Button} from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text: "John PREFETCH"}))
+const Page = () => {
+  // const queryClient = getQueryClient();
+  // void queryClient.prefetchQuery(trpc.createAI.queryOptions({text: "John PREFETCH"}))
   // fetch data from a server component
   // const data = await caller.createAI({text: "My server"});
   // fetch data from the api, this is the data access layer
   // const { data } = useQuery(trpc.createAI.queryOptions({text: "John"}));
 
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Client /> 
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={() => invoke.mutate({text: "Test"})}>
+        Invoke Background Job
+      </Button>
+    </div>
   );
 }
 
