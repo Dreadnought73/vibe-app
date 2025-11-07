45280945c3edef4952adb54f4471e392723edfdc
Armin Banfalvi <abanfalvi@yahoo.com>
2025-11-07 22:03:21 +0100

    03:trpc-setup

--- vibe/src/app/page.tsx
+++ vibe/src/app/page.tsx
@@ -1,10 +1,23 @@
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 
-const Page = () => {
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text: "John PREFETCH"}))
+  // fetch data from a server component
+  // const data = await caller.createAI({text: "My server"});
+  // fetch data from the api, this is the data access layer
+  // const { data } = useQuery(trpc.createAI.queryOptions({text: "John"}));
+
   return (
-    <div>
-      Hello world
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client /> 
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 
