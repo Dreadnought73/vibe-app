a07bc5bf0139838e83de62b1f3650d198f373361
Armin Banfalvi <abanfalvi@yahoo.com>
2025-11-08 11:26:44 +0100

    05: ai-jobs

--- vibe/src/app/page.tsx
+++ vibe/src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import {Button} from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 const Page = () => {
   // const queryClient = getQueryClient();
@@ -11,13 +13,14 @@ const Page = () => {
   // const data = await caller.createAI({text: "My server"});
   // fetch data from the api, this is the data access layer
   // const { data } = useQuery(trpc.createAI.queryOptions({text: "John"}));
-
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({}));
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button onClick={() => invoke.mutate({text: "Test"})}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)}/>
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({value: value})}>
         Invoke Background Job
       </Button>
     </div>
