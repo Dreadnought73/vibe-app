8bc70b8be261a2ae40ff9d5e73ec17f7bc88b9e8
Armin Banfalvi <abanfalvi@yahoo.com>
2025-11-08 19:44:26 +0100

    06: e2b sandboxes

--- vibe/src/inngest/functions.ts
+++ vibe/src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from '@e2b/code-interpreter';
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-armin-123-2");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer.  You write readable and maintable code. You write simple next.js & react snippets.",
@@ -12,9 +19,15 @@ export const helloWorld = inngest.createFunction(
     });
 
     const { output } = await codeAgent.run(
-    `Write the following snippets ${event.data.value}`,
-  );
+      `Write the following snippets: ${event.data.value}`,
+    );
 
-    return { output};
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
+    return { output, sandboxUrl };
   },
 );
