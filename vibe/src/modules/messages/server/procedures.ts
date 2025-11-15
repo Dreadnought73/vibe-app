46dc9e86d07ebdc4c4a688f0322cfb85569dccda
Armin Banfalvi <abanfalvi@yahoo.com>
2025-11-15 12:47:34 +0100

    16: authentication

--- vibe/src/modules/messages/server/procedures.ts
+++ vibe/src/modules/messages/server/procedures.ts
@@ -1,19 +1,24 @@
 import { inngest } from "@/inngest/client";
 import { prisma } from "@/lib/prisma";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import {z} from "zod";
 // this will be access via message.create()
 export const messagesRouter = createTRPCRouter({
-    getMany: baseProcedure
+    getMany: protectedProcedure
     .input(
             z.object({
                 projectId: z.string().min(1, {message: "Project ID is required"}),
             }),
         )
-        .query(async({input}) => {
+        .query(async({input, ctx}) => {
             const messages = await prisma.message.findMany({
                 where: {
                     projectId: input.projectId,
+                    // we have to go through the project id first since no direct user id through the message
+                    project: {
+                        userId: ctx.auth.userId,
+                    }
                 },
                 include: {
                     fragment: true,
@@ -25,7 +30,7 @@ export const messagesRouter = createTRPCRouter({
         
             return messages
         }),
-    create: baseProcedure
+    create: protectedProcedure
         .input(
             z.object({
                 value: z.string()
@@ -34,11 +39,21 @@ export const messagesRouter = createTRPCRouter({
                 projectId: z.string().min(1, {message: "Project ID is required"}),
             }),
         )
-        .mutation(async ({input}) => {
+        .mutation(async ({input, ctx}) => {
+            const existingProject = await prisma.project.findUnique({
+                where: {
+                    id: input.projectId,
+                    userId: ctx.auth.userId,
+                },
+            });
+
+            if (!existingProject) {
+                throw new TRPCError({code: 'NOT_FOUND', message: "Project not found"})
+            }
             const createdMessage = await prisma.message.create({
                 data: {
                     // each project would have a new ID
-                    projectId: input.projectId,
+                    projectId: existingProject.id,
                     content: input.value,
                     role: "USER",
                     type: "RESULT",
--- vibe/src/modules/projects/ui/views/project-view.tsx
+++ vibe/src/modules/projects/ui/views/project-view.tsx
@@ -10,6 +10,7 @@ import { FragmentWeb } from "../components/fragment-web";
 import { CodeIcon, CrownIcon, EyeIcon, Link } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { FileExplorer } from "@/components/file-explorer";
+import { UserControl } from "@/components/user-control";
 
 interface Props {
     projectId: string
@@ -60,6 +61,7 @@ export const ProjectView = ({projectId}: Props) => {
                                         <CrownIcon /> Upgrade
                                     </Link>
                                 </Button>
+                                <UserControl />
                             </div>
                         </div>
                         <TabsContent value="preview">
